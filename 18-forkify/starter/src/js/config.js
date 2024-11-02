1a66c619c8d9907000243bb424898befd7b31256
Peter Son <stayinvestor@gmail.com>
2024-11-02 13:16:19 -0400

    Update on chapter 18 - Forkify App - Building a Modern Application project

---
+++ 18-forkify/starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'efdc6168-622b-4af3-8c74-131873e31b27';
+export const MODAL_CLOSE_SEC = 2.5;
